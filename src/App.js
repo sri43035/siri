e3ff3a6f8b517f365f4a3ae43a8d1e9347d5c94f
Sridhar0435 <ssri668@gmail.com>
2020-08-14 23:09:23 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import Deck from "./components/Deck";
+import yara from "./styles/css/yara.mp3";
+let audio = new Audio(yara);
+const start = () => {
+  alert("Happy Birthday siri !!!😍😍 i Miss you so muchh");
+  audio.play();
+};
+const App = () => (
+  <>
+    <Deck />
+    <button onClick={start}> Oi SIRI click me</button>
+  </>
+);
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,110 @@
+import b1 from "./styles/css/b1.png";
+import gif from "./styles/css/wish.gif";
+import queen from "./styles/css/queen.webp";
+import saree1 from "./styles/css/saree1.jpg";
+import saree2 from "./styles/css/saree2.png";
+import saree3 from "./styles/css/saree3.png";
+import cutie1 from "./styles/css/cutie1.jpg";
+import cutie2 from "./styles/css/cutie2.jpg";
+import cutie3 from "./styles/css/cutie3.jpg";
+import trade1 from "./styles/css/trade1.jpg";
+import trade2 from "./styles/css/trade2.jpg";
+import angel2 from "./styles/css/angel2.jpg";
+import angel3 from "./styles/css/angel3.jpg";
+import t1 from "./styles/css/t1.jpg";
+import ash1 from "./styles/css/ash1.jpg";
+import pink2 from "./styles/css/pink2.jpg";
+import clg1 from "./styles/css/clg1.jpg";
+import clg2 from "./styles/css/clg2.jpg";
+import clg3 from "./styles/css/clg3.jpg";
+import clgsaree1e from "./styles/css/clgsaree1e.jpg";
+import clgsaree2e from "./styles/css/clgsaree2e.jpg";
+import clgsaree3e from "./styles/css/clgsaree3e.jpg";
+import smiling from "./styles/css/smiling.jpg";
+export default [
+  {
+    pics: [smiling],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Keep smiling siri.😍",
+  },
+  {
+    pics: [cutie2],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "whatever you where you just look soo good.😍",
+  },
+  {
+    pics: [cutie3],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Siriiii with cute smile. 😍 🤩",
+  },
+  {
+    pics: [clg1, clg2, clg3],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Pics in college and best place to be. 😍",
+  },
+  {
+    pics: [saree2, saree1, saree3],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "You look more beautifull in sarees ",
+  },
+  {
+    pics: [b1, trade1, trade2],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Ne dressing wow 😋!!",
+  },
+  {
+    pics: [clgsaree1e, clgsaree2e, clgsaree3e],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Saree lo kuda masth 😍😍😍.",
+  },
+  {
+    pics: [angel2, angel3],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Always cute.",
+  },
+  {
+    pics: [pink2],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Hope you birthday is amazing.",
+  },
+  {
+    pics: [cutie1, t1],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Cute Smile .",
+  },
+  {
+    pics: [ash1],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text: "Ashhhhhhhhhhhh color dress 😍😍😍",
+  },
+  {
+    pics: [gif, saree1],
+    name: "Siri",
+    age: "15 Aug",
+    distance: "",
+    text:
+      "Wish you many more Happy Returns of the day Siri! You are the best person. Keep smiling an be cute as always 😍",
+  },
+];
